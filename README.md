d074a53ad96ad02e19a746d13a604b7887fc631e
jarewat janthadee <61160135@go.buu.ac.th>
2021-09-05 17:23:00 +0700

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# ButterCMS Express.js Example
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+After deploying to Heroku, email support@buttercms.com for access to the demo account.
+
+This app uses EJS templates but ButterCMS works with any templating engine like Jade, Mustache, or React.
+
+## Get Started
+
+Install dependencies:
+
+```
+$ npm install
+```
+
+Start the app:
+
+```
+$ npm start
+```
+
+## Documentation
+
+For a comprehensive list of examples and API docs, check out our [documentation](https://buttercms.com/docs/).
--- app.js
+++ app.js
@@ -1,36 +1,132 @@
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
+'use strict';
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// module.exports = app;
-
-// create an express app
-const express = require("express")
+const express = require('express');
+const butter = require('buttercms')('f1cab14794d33eadb2cde1165c2651e8872f2942');
 const app = express()
 
-// use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+// Routes
+app.get('/', renderHome)
+app.get('/blog', renderBlogHome)
+app.get('/blog/:slug', renderPost)
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.get('/category/:slug', renderCategory)
+app.get('/author/:slug', renderAuthor)
+
+app.get('/rss', renderRss)
+app.get('/atom', renderAtom)
+app.get('/sitemap', renderSitemap)
+
+// Location Pages in Butter
+app.get('/locations/:slug', renderPage)
+
+// Start server
+app.listen(process.env.PORT || 3000)
+
+
+function renderPage(req, res) {
+  var slug = req.params.slug || 1;
+
+  butter.content.retrieve(['location_pages[slug='+slug+']']).then(function(resp) {
+    var content = resp.data.data;
+
+    // location_pages is a collection so we access the first and only item
+    var location = content.location_pages[0];
+
+    res.render('location', {
+      location: location
+    })
+  });
+}
+
+function renderHome(req, res) {
+
+  // Display list of location pages on our homepage.
+  butter.content.retrieve(['location_pages']).then(function(resp) {
+    var content = resp.data.data;
+    var location_pages = content.location_pages;
+
+    butter.post.list({page_size: 4, page: 1}).then(function(postResp) {
+      var posts = postResp.data.data;
+      
+      res.render('index', {
+        locations: location_pages,
+        posts: posts
+      })
+    });
+  });
+}
+
+function renderBlogHome(req, res) {
+  var page = req.params.page || 1;
+
+  butter.post.list({page_size: 10, page: page}).then(function(resp) {
+    res.render('bloghome', {
+      posts: resp.data.data,
+      next_page: resp.data.meta.next_page,
+      previous_page: resp.data.meta.previous_page
+    })
+  })
+}
+
+function renderPost(req, res) {
+  var slug = req.params.slug;
+
+  butter.post.retrieve(slug).then(function(resp) {
+    res.render('post', {
+      title: resp.data.data.title,
+      post: resp.data.data,
+      published: new Date(resp.data.data.published)
+    })
+  })
+}
+
+function renderAuthor(req, res) {
+  var slug = req.params.slug;
+
+  butter.author.retrieve(slug, {include: 'recent_posts'}).
+    then(function(resp) {
+      res.render('author', {
+        title: `${resp.data.data.first_name} ${resp.data.data.last_name}`,
+        author: resp.data.data
+      })
+    })
+}
+
+function renderCategory(req, res) {
+  var slug = req.params.slug;
+
+  butter.category.retrieve(slug, {include: 'recent_posts'})
+    .then(function(resp) {
+      res.render('category', {
+        title: resp.data.data.name,
+        category: resp.data.data
+      })
+    })
+}
+
+function renderAtom(req, res) {
+  res.set('Content-Type', 'text/xml');
+
+  butter.feed.retrieve('atom').then(function(resp) {
+    res.send(resp.data.data)
+  })
+}
+
+function renderRss(req, res) {
+  res.set('Content-Type', 'text/xml');
+
+  butter.feed.retrieve('rss').then(function(resp) {
+    res.send(resp.data.data)
+  })
+}
+
+function renderSitemap(req, res) {
+  res.set('Content-Type', 'text/xml');
+
+  butter.feed.retrieve('sitemap').then(function(resp) {
+    res.send(resp.data.data)
+  })
+}
